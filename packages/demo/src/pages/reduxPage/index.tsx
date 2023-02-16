96744f5f26a2c1572d5283a475c263c661d0e451
jerry-lllman <jerry_lllman@163.com>
2023-02-16 16:55:03 +0800

    redux core code

--- packages/demo/src/pages/reduxPage/index.tsx
+++ packages/demo/src/pages/reduxPage/index.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useReducer } from "react"
 import store from "../../store"
 
 export default function ReduxPage() {
 
 	const count = store.getState()
+
+	const [, forceUpdate] = useReducer((state) => state + 1, 0)
+
+	useEffect(() => {
+		const unsubscribe = store.subscribe(forceUpdate)
+
+		return () => {
+			unsubscribe()
+		}
+	}, [])
 
 	const add = () => {
 		store.dispatch({ type: 'ADD' })
--- packages/redux/src/createStore.ts
+++ packages/redux/src/createStore.ts
@@ -4,19 +4,27 @@ const initType = Symbol(+new Date())
 export default function createStore(reducer: any) {
 
 	let currentState: any
+	let listeners: Function[] = []
 
-	function getState(){
+	function getState() {
 		return currentState
 	}
 
 	function dispatch(action: any) {
 		currentState = reducer(currentState, action)
+		listeners.forEach(listener => listener())
 	}
 
-	function subscribe() {
+	function subscribe(listener: Function) {
 
+		listeners.push(listener)
+
+		return () => {
+			listeners = listeners.filter(fn => fn !== listener)
+		}
 	}
 
+	// redux init
 	dispatch({ type: initType })
 
 	return {
